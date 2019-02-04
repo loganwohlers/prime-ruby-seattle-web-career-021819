fd63f6c58689926962bcf70dab9120f3600456c9
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 00:20:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(n)
-  a=[2,3,4,5,6,7,8,9]
+  
   if(n<0)
     n=n*-1
   end
@@ -7,7 +7,7 @@ def prime?(n)
   if (n==1)
     return false
   end
-  
+  a=
   a.each do |num|
     if(n%num==0 && n!=num)
       return false
