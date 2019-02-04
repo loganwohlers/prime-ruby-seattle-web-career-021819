3e78cf48dc67b88af76d2986b52192ef617f94d5
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 00:23:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  
+  if(n<0)
+    n=n*-1
+  end
+  
+  if (n==1||n==0)
+    return false
+  end
+  a=(2..(n-1)).to_a
+  a.each do |num|
+    if(n%num==0 && n!=num)
+      return false
+    end
+  end
+  true
+end
